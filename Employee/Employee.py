1ce00c129039194892c0bd6f1cf68f35aa560722
Anna Sigrun <annasg19@ru.is>
2019-12-03 16:09:56 +0000

    fokk já

--- Employee/Employee.py
+++ Employee/Employee.py
@@ -15,25 +15,24 @@ class Employee(object):
         '''Takes an instance of an employee and saves it in an employee file.
         If such file doesn't exist, it's created.'''
         with open("employees.csv", "a") as employees:
-            list_of_attributes = [self.__ssn, self.__name, self.__role, self.__rank, self.address, self.mobile, self.email]
+            list_of_attributes = [self.__ssn, self.__name, self.__role, self.__rank, self.licence, self.address, self.mobile, self.email]
             employees.write(','.join(list_of_attributes) + '\n')
 
     def change_employee(self):
         pass
-
+    
     def __str__(self):
         '''Returns the employee information on a very pretty format'''
         the_line = "{:<20s}Licence: {}\n{:<20s}Role: {}\n{:<20s}Rank: {}\n{}\n{}".format(self.__ssn, self.licence, self.__name, self.__role, self.address, self.__rank, self.mobile, self.email)
         return the_line
 
-
-def find_employee(ssn):
+def find_employee(some_variable, search_key):
     '''Takes a ssn and finds the corresponding line in the employees.csv file.
     Returns the line in an instance of Employee.'''
     with open("employees.csv") as employees:
         employee_reader = csv.DictReader(employees)
         for row in employee_reader:
-            if ssn == row['ssn']:
+            if some_variable == row[search_key]:
                 return Employee(row['ssn'], row['name'], row['role'], row['rank'], row['licence'], row['address'], row['phonenumber'], row['email'])
         return 'Employee does not exist!'
 
@@ -41,4 +40,4 @@ def find_employee(ssn):
 # input("Press enter to save John to file: ")
 # john.save_employee()
 
-# print(find_employee('0000003322'))
+print(find_employee('1501681497'))
