44e401cb67e50034df5aec718404288606fec1dd
Anna Sigrun <annasg19@ru.is>
2019-12-03 12:42:16 +0000

    idk at this point

--- Employee/Employee.py
+++ Employee/Employee.py
@@ -14,7 +14,7 @@ class Employee(object):
     def save_employee(self):
         '''Takes an instance of an employee and saves it in an employee file.
         If such file doesn't exist, it's created.'''
-        employees = open("employees.csv", "a+")
+        employees = open("employees.csv", "a")
         list_of_attributes = [self.__ssn, self.__name, self.__role, self.__rank, self.address, self.mobile, self.email]
         employees.write(','.join(list_of_attributes) + '\n')
         employees.close()
@@ -26,6 +26,6 @@ class Employee(object):
 def find_employee(ssn):
     employees = open("employees.csv")
 
-john = Employee('100382-2389', 'John Stevenson', 'Pilot', 'Captain', 'Boeing', '5686802', 'Flugmannavegur 3', 'refur34@gmail.com')
+john = Employee('100382-2389', 'John Stevenson Jr', 'Pilot', 'Captain', 'Boeing', '5686802', 'Flugmannavegur 3', 'refur34@gmail.com')
 input("Press enter to save John to file: ")
 john.save_employee()
