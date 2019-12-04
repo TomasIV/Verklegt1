a46add46c6a5c66caf2bf88a9456e7b1352707f6
Breki Ben <brekiben@Brekis-MacBook-Pro.local>
2019-12-04 14:03:13 +0000

    Breki

--- Models/Voyage.py
+++ Models/Voyage.py
@@ -2,11 +2,11 @@ import datetime
 import dateutil.parser
 
 class Voyage:
-    def __init__(self, status, sold_seats, airplain, employee_ssn, date):
+    def __init__(self, status, sold_seats, airplain, date):
         self.status = status
         self.sold_seats = sold_seats
         self.airplain = airplain
-        self.employee_ssn = employee_ssn
+        self.employees = []
         self.date = dateutil.parser.parse(date)
 
 
@@ -15,14 +15,17 @@ class Voyage:
         status = input("Status: ")
         sold_seats = input("Sold Seats: ")
         airplain = input("Plane Insignia: ")
-        employee_ssn = input("Employee SSN: ")
         year = input("Year: ")
         month = input("Month: ")
         day = input("Day: ")
         hour = input("Hour: ")
         minute = input("Minute: ")
         date = datetime.datetime(year,month,day,hour,minute,0).isoformat()
-        return Voyage(status, sold_seats, airplain, employee_ssn, date)
+        return Voyage(status, sold_seats, airplain, date)
         print("Wow! you created an voyage!")
+
+
+    def add_employee_to_voyage(self, ssn):
+        self.employees.append(ssn)
 
 
