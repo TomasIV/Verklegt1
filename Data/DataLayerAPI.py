c6030074b1294fe60c9f53d5a5ae2286512f3f72
Tómas Ingi Villalobos <tomasingiv@hotmail.com>
2019-12-04 15:58:32 +0000

    :)

--- Data/DataLayerAPI.py
+++ Data/DataLayerAPI.py
@@ -9,7 +9,7 @@ class DataLayer:
         '''Takes an instance of an employee and saves it in an employee file.
         If such file doesn't exist, it's created.'''
         self.some_employee = some_employee
-        with open("Verklegt1\Data\Crew.csv", "a") as employees:
+        with open("Data\Crew.csv", "a") as employees:
             csv_writer = csv.writer(employees)
             #list_of_attributes = [self.some_employee.ssn, self.some_employee.name, self.some_employee.role, self.some_employee.rank, self.some_employee.licence, self.some_employee.address, self.some_employee.mobile, self.some_employee.email]
             csv_writer.writerow(some_employee)
