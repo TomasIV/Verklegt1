e55147caeaed34b986c0c9705330e99c3d1beb12
Anna Sigrun <annasg19@ru.is>
2019-12-04 19:26:23 +0000

    Panna's commit

--- Data/EmployeeDL.py
+++ Data/EmployeeDL.py
@@ -11,7 +11,7 @@ class EmployeeDL:
         '''Takes an instance of an employee and saves it in an employee file.
         If such file doesn't exist, it's created.'''
         self.some_employee = some_employee
-        with open("Data\employees.csv", "a") as employees:
+        with open("CSVFiles\Employees.csv", "a") as employees:
             csv_writer = csv.writer(employees)
             #list_of_attributes = [self.some_employee.ssn, self.some_employee.name, self.some_employee.role, self.some_employee.rank, self.some_employee.licence, self.some_employee.address, self.some_employee.mobile, self.some_employee.email]
-            csv_writer.writerow(some_employee)
+            csv_writer.writerow(some_employee.get_employee_attributes())
