48ac53fcf86f96bc344328231a4a996c9746b50f
andriberg19 <andri19@ru.is>
2019-12-04 10:38:00 +0000

    Lista employee

--- Data/DataLayer.py
+++ Data/DataLayer.py
@@ -12,3 +12,20 @@ class DataLayer:
             csv_writer = csv.writer(employees)
             list_of_attributes = [some_employee.__ssn, some_employee.__name, some_employee.__role, some_employee.__rank, some_employee.licence, some_employee.address, some_employee.mobile, some_employee.email]
             csv_writer.writerow(list_of_attributes)
+
+    def list_employee():
+        list_employee = []
+        with open('employees.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                employee = Employee(row['ssn'], row['name'], row['role'], row['rank'], row['licence'], row['address'], row['phonenumber'], row['email'])
+                list_employee.append(employee)
+            #return list_employee
+            print(list_employee)
+
+
+
+john = Employee('100382-2389', 'steb stebson', 'Pilot', 'Captain', 'Boeing', '5686802', 'Flugmannavegur 3', 'refur34@gmail.com')
+input("Press enter to save John to file: ")
+#john.save_employee()
+Employee.list_employee()
