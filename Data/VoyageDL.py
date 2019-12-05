e0a22af1b8a6f64348026236a2ca7e2ca055f8cd
Breki Ben <brekiben@Brekis-MacBook-Pro.local>
2019-12-05 11:54:42 +0000

    ehv

--- Data/VoyageDL.py
+++ Data/VoyageDL.py
@@ -1,2 +1,10 @@
 from Logic.LogicLayerAPI import LogicLayer
 from Models.Voyage import Voyage
+
+class Voyage:
+    def __init__(self):
+
+    def save_voyage(self):
+
+    def list_voyage(self):
+        
