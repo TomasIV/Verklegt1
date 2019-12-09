7ac2876b24b05fe52144f237859937fdd9395fb3
Anna Sigrun <annasg19@ru.is>
2019-12-09 18:02:50 +0000

    I did the thing

--- Data/VoyageDL.py
+++ Data/VoyageDL.py
@@ -6,18 +6,18 @@ class VoyageDL:
     def __init__(self):
         pass
 
-    def save_voyage(self, some_voyage):
-        with open(self.PATH, "a") as voyage:
-            csv_writer = csv.writer(voyage, lineterminator= "\r" )
-            csv_writer.writerow(some_voyage.get_voyage_attributes())
-
     def list_voyages(self):
-        list_aircraftType = []
+        list_of_voyages = []
         with open(self.PATH, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
-            for row in reader: 
-                voyage = Voyage(row['planeTypeId'], row['manufacturer'], row['model'],row['capacity'], row['emptyWheight'], row['maxTakeoffWeight'], row['unitThrust'], row['serviceCeiling'], row['length'], row['height'], row['wingspan'])
-                list_aircraftType.append(voyage)
-            #þarf að laga // vantar enþá nokkrar upplýsingar 
-            #return list_aircraftType
-            print(list_aircraftType)
+            for row in reader:
+                voyage = Voyage(row['destination'], row['departure'], row['aircraftID'], row['soldSeats'], [row['flightNumber1'], row['flightNumber2']], [row['captain'], row['copilot'], row['fsm'], row['fa1'], row['fa2']])
+                list_of_voyages.append(voyage)
+            return list_of_voyages
+    
+    def overwrite_file(self, list_of_voyages):
+        with open(self.PATH, "w") as cleared_file:
+            overwriter = csv.writer(cleared_file, lineterminator= "\r")
+            overwriter.writerow(['flightNumber1', 'flightNumber2', 'destination', 'departure', 'aircraftID', 'soldSeats', 'captain', 'copilot', 'fsm', 'fa1', 'fa2'])
+            for path in list_of_voyages:
+                overwriter.writerow(path.get_voyage_attributes())
--- Models/VoyageMODEL.py
+++ Models/VoyageMODEL.py
@@ -2,19 +2,19 @@ import datetime
 import dateutil.parser
 
 class Voyage:
-    def __init__(self, destination, date, airplane, sold_seats):
-        self.__flight_numbers = []
+    def __init__(self, destination, date, airplane, sold_seats, flight_numbers = [], employees = []):
+        self.__flight_numbers = flight_numbers
         self.__destination_id = destination
         self.__departure = date
         self.__aircraft_id = airplane
         self.sold_seats = sold_seats
-        self.employees = []
+        self.employees = employees
 
     def get_voyage_attributes(self):
         return [self.__flight_numbers[0], self.__flight_numbers[1], self.__destination_id, self.__departure, self.__aircraft_id, self.sold_seats, self.employees[0], self.employees[1], self.employees[2], self.employees[3], self.employees[4]]
 
-
-    def create_voyage(self):
+    # Þetta fall á að vera í interfacinu
+    '''def create_voyage(self):
         print("Please enter the details of the new voyage")
         #status = input("Status: ") Status á nýju voyage????
         sold_seats = input("Sold Seats: ")
@@ -27,16 +27,17 @@ class Voyage:
         date = dateutil.parser.parse(datetime.datetime(year,month,day,hour,minute,0).isoformat())
         input ("{} :)".format(date))
         return Voyage(sold_seats, airplane, date)
-        #status
+        #status'''
 
     def add_employee_to_voyage(self, ssn):
         self.employees.append(ssn)
 
-    def __eq__(self, comparison): #LAGA
-        if self.sold_seats == comparison \
-        or self.airplane == comparison \
-        or self.date == comparison \
-        or comparison in self.__flight_numbers:
+    def __eq__(self, comparison):
+        if comparison in self.__flight_numbers \
+        or self.__destination_id == comparison \
+        or self.__departure == comparison \
+        or self.__aircraft_id == comparison \
+        or comparison in self.employees:
             return True
         else:
             return False
