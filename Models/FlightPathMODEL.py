45dc1e7bb5751e9a4c9448d72c4bdddbbb3a504d
Anna Sigrun <annasg19@ru.is>
2019-12-09 15:13:59 +0000

    FlightPathMODEL ready

--- Models/FlightPathMODEL.py
+++ Models/FlightPathMODEL.py
@@ -9,8 +9,16 @@ class FlighPath:
         self.__departure_timestamp = datetime.datetime(int(departure_year), int(departure_month), int(departure_day), int(departure_hour), int(departure_minute), 0).isoformat
         self.__arrival_timestamp = ''
     
-    def set_arrival_time(length_of_fight):
-        
+    def set_arrival_time(self, length_of_fight):
+        minutes = int(length_of_fight)
+        self.__arrival_timestamp = self.__departure_timestamp + datetime.timedelta(minutes,0)
+    
+    def set_flight_number(self, flight_number):
+        self.__flight_number = flight_number
 
     def get_flightpath_attributes(self):
         return [self.__flight_number, self.__departing_id, self.__destination_id, self.__departure_timestamp, self.__arrival_timestamp]
+
+    def __str__(self):
+        the_line = "Flightnumber: {}   Departing from: {}   Arriving at: {}\nDeparture: {}   Arrival: {}".format(self.__flight_number, self.__departing_id, self.__destination_id, self.__departure_timestamp, self.__arrival_timestamp)
+        return the_line
