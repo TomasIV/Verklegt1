ed6ab7519f143a4f63bb94109b269e2f02cda52b
Anna Sigrun <annasg19@ru.is>
2019-12-09 14:26:50 +0000

    Panna's commit

--- Models/FlightPathMODEL.py
+++ Models/FlightPathMODEL.py
@@ -2,7 +2,11 @@ import datetime
 import dateutil.parser
 
 class FlighPath:
-    def __init__(self):
-        self.__flight_number = ''
-        self.__departing_id = ''
-        self.__destination_id = ''
+    def __init__(self, flightnumber, departing_location, destination):
+        self.__flight_number = flightnumber
+        self.__departing_id = departing_location
+        self.__destination_id = destination
+    
+    def get_flightpath_attributes(self):
+        return [self.__flight_number, self.__departing_id, self.__destination_id, ]
+        # flightNumber,departingFrom,arrivingAt,departure,arrival
