6b508be1dfb256e838e814157d492cad63e63f90
Tómas Ingi Villalobos <tomasingiv@hotmail.com>
2019-12-11 22:49:01 +0000

    wasadu

--- Data/AircraftModelDL.py
+++ Data/AircraftModelDL.py
@@ -8,7 +8,7 @@ class PlaneModel:
 
     def save_model(self, some_aircraftType):
          with open(self.PATH, "a", encoding="utf-8") as aircraftType:
-            csv_writer = csv.writer(aircraftType, lineterminator= "\r" )
+            csv_writer = csv.writer(aircraftType, lineterminator= "\r")
             csv_writer.writerow(some_aircraftType.get_aircraftType_attributes())
 
 #planeTypeId,manufacturer,model,capacity,emptyWeight,maxTakeoffWeight,unitThrust,serviceCeiling,length,height,wingspan
