f15a10a4f3b172f6acef5837493522bd263807be
Anna Sigrun <annasg19@ru.is>
2019-12-13 21:49:55 +0000

    Commenting

--- Models/DestinationMODEL.py
+++ Models/DestinationMODEL.py
@@ -1,4 +1,6 @@
 class Destination:
+    '''Simple destination class.
+    It has seven attributes (besides "self").'''
     def __init__(self, destination_id, destination, destination_number, emergency_contact, emergency_phone, flight_time, kilometers):
         self.__id = destination_id
         self.__destination = destination
@@ -7,18 +9,11 @@ class Destination:
         self.emergency_phone = emergency_phone
         self.__flight_time = flight_time
         self.__kilometers = kilometers
-
-    def get_destination_attributes(self):
-        return [self.__id, self.__destination, self.__number, self.emergency_contact, self.emergency_phone, self.__flight_time, self.__kilometers]
-
-    def get_destiantion_number(self):
-        return self.__number
-    
-    def get_fligh_time(self):
-        return self.__flight_time
-    
-    def get_name(self):
-        return self.__destination
+        
+    def __str__(self):
+        '''Returns the destinastion information format so it looks nice and clean'''
+        attribute = ["Destination", "Destination ID", "Destination Number", "Emergency Contact", "Emergency phone number", "Flight time", "Kilometers"] #Longest string is 23 so spacing is 23
+        return ("{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n".format(attribute[0], self.__destination, attribute[1], self.__id, attribute[2], self.__number, attribute[3], self.emergency_contact, attribute[4], self.emergency_phone, attribute[5], self.__flight_time, attribute[6], self.__kilometers))
     
     def __eq__(self, comparison):
         if self.__id == comparison \
@@ -31,8 +26,19 @@ class Destination:
             return True
         else:
             return False
-        
-    def __str__(self):
-        '''Returns the destinastion information format so it looks super nice and pretty'''
-        attribute = ["Destination", "Destination ID", "Destination Number", "Emergency Contact", "Emergency phone number", "Flight time", "Kilometers"] #Longest string is 23 so spacing is 23
-        return ("{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n{:<23s}: {}\n".format(attribute[0], self.__destination, attribute[1], self.__id, attribute[2], self.__number, attribute[3], self.emergency_contact, attribute[4], self.emergency_phone, attribute[5], self.__flight_time, attribute[6], self.__kilometers))
+
+    def get_destination_attributes(self):
+        '''Returnes the destination attributes in a list'''
+        return [self.__id, self.__destination, self.__number, self.emergency_contact, self.emergency_phone, self.__flight_time, self.__kilometers]
+
+    def get_destiantion_number(self):
+        '''Returns the destination number'''
+        return self.__number
+    
+    def get_fligh_time(self):
+        '''Returns the flight time to the destiantion'''
+        return self.__flight_time
+    
+    def get_name(self):
+        '''Returns the name of the destination'''
+        return self.__destination
--- Models/VoyageMODEL.py
+++ Models/VoyageMODEL.py
@@ -2,6 +2,8 @@ import datetime
 import dateutil.parser
 
 class Voyage:
+    '''Voyage class.
+    It has 17 variables (besides "self"), 11 of which have a default value.'''
     def __init__(self, airplane, destination, sold_seats_out, sold_seats_back, \
             first_departure_date, first_arrival_date = '', second_departure_date = '', second_arrival_date = '', \
             home_airport = 'KEF', flight_number_1 = '', flight_number_2 = '', captain = '', copilot = '', fsm = '', fa1 = '', fa2 = ''):
@@ -24,7 +26,7 @@ class Voyage:
         self.seats = 0
 
     def __str__(self):
-        '''Very very long string'''
+        '''Returnes all attributes on a very readable string format'''
         if self.captain != '' and self.copilot != '' and self.fsm != '':
             manned_or_not = 'Fully manned'
         else:
@@ -39,62 +41,6 @@ class Voyage:
             attributes[8], self.fsm, attributes[4], str(self.seats - int(self.first_sold_seats)), attributes[4], str(self.seats - int(self.second_sold_seats)), \
             attributes[9], self.fa1, attributes[5], self.first_sold_seats, attributes[5], self.second_sold_seats, \
             attributes[9], self.fa2)
-
-    def get_identification(self):
-        one = ("\n{:<30s}{:<30s}{:<30s}{:<30s}{:<30s}".format("From/To", "Flight number", "Departure", "Arrival", "Sold seats"))
-        two = ("{:<30s}{:<30s}{:<30s}{:<30s}{:<30s}".format("From ICE To " + self.__destination_id, self.first_flight_number, self.__first_departure, self.__first_arrival, self.first_sold_seats))
-        three = ("{:<30s}{:<30s}{:<30s}{:<30s}{:<30s}\n".format("From " + self.__destination_id + " To ICE", self.second_flight_number, self.__second_departure, self.__second_arrival, self.second_sold_seats))
-        return (one, two, three)
-
-    def get_employees_on_voyage(self):
-        return [self.captain, self.copilot, self.fsm, self.fa1, self.fa2]
-
-    def get_destination(self):
-        return self.__destination_id
-    
-    def get_home_airport(self):
-        return self.__home_id
-
-    def get_voyage_flight_numbers(self):
-        return (self.first_flight_number, self.second_flight_number)
-    
-    def get_voyage_plane_id(self):
-        return self.__aircraft_id
-
-    def get_voyage_depart_time(self):
-        return self.__first_departure
-
-    def get_takeoff_dates(self):
-        return [self.__first_departure, self.__first_arrival, self.__second_departure, self.__second_arrival]
-
-    def get_voyage_attributes(self):
-        return [self.__aircraft_id, self.__home_id, self.__destination_id, \
-            self.first_flight_number, self.first_sold_seats, self.__first_departure, self.__first_arrival, \
-            self.second_flight_number, self.second_sold_seats, self.__second_departure, self.__second_arrival, \
-            self.captain, self.copilot, self.fsm, self.fa1, self.fa2]
-    
-    def add_dates_to_voyage(self, first_arrival_date, second_departure_date, second_arrival_date):
-        self.__first_arrival = first_arrival_date
-        self.__second_departure = second_departure_date
-        self.__second_arrival = second_arrival_date
-    
-    def add_flight_numbers_to_voyage(self, num1, num2):
-        self.first_flight_number = num1
-        self.second_flight_number = num2
-    
-    def add_number_of_seats(self, number_of_seats):
-        self.seats = number_of_seats
-
-    def change_flight_numbers(self):
-        '''Takes an instance of a Voyage and raises the last number in the flight numbers by 2'''
-        last_num_1 = int(self.first_flight_number[4])
-        last_num_2 = int(self.second_flight_number[4])
-
-        last_char_1 = str(last_num_1 + 2)
-        last_char_2 = str(last_num_2 + 2)
-
-        self.first_flight_number = self.first_flight_number[:4] + last_char_1
-        self.second_flight_number = self.second_flight_number[:4] + last_char_2
 
     def __eq__(self, comparison):
         if self.__aircraft_id == comparison \
@@ -111,8 +57,77 @@ class Voyage:
         else:
             return False
 
+    def get_identification(self):
+        one = ("\n{:<30s}{:<30s}{:<30s}{:<30s}{:<30s}".format("From/To", "Flight number", "Departure", "Arrival", "Sold seats"))
+        two = ("{:<30s}{:<30s}{:<30s}{:<30s}{:<30s}".format("From ICE To " + self.__destination_id, self.first_flight_number, self.__first_departure, self.__first_arrival, self.first_sold_seats))
+        three = ("{:<30s}{:<30s}{:<30s}{:<30s}{:<30s}\n".format("From " + self.__destination_id + " To ICE", self.second_flight_number, self.__second_departure, self.__second_arrival, self.second_sold_seats))
+        return (one, two, three)
+
+    def get_voyage_attributes(self):
+        '''Returnes the voyage attributes in a list'''
+        return [self.__aircraft_id, self.__home_id, self.__destination_id, \
+            self.first_flight_number, self.first_sold_seats, self.__first_departure, self.__first_arrival, \
+            self.second_flight_number, self.second_sold_seats, self.__second_departure, self.__second_arrival, \
+            self.captain, self.copilot, self.fsm, self.fa1, self.fa2]
+
+    def get_employees_on_voyage(self):
+        '''Returnes the employee social security numbers in a list'''
+        return [self.captain, self.copilot, self.fsm, self.fa1, self.fa2]
+
+    def get_destination(self):
+        '''Returnes the destination id of a voyage'''
+        return self.__destination_id
+    
+    def get_home_airport(self):
+        '''Returnes the home airport'''
+        return self.__home_id
+
+    def get_voyage_flight_numbers(self):
+        '''Returnes the flight numbers of a voyage'''
+        return (self.first_flight_number, self.second_flight_number)
+    
+    def get_voyage_plane_id(self):
+        '''Returnes the plane id of a voyage'''
+        return self.__aircraft_id
+
+    def get_voyage_depart_time(self):
+        '''Returnes the first departure time of a voyage on iso format'''
+        return self.__first_departure
+
+    def get_takeoff_dates(self):
+        '''Returnes the takeoff dates in a list'''
+        return [self.__first_departure, self.__first_arrival, self.__second_departure, self.__second_arrival]
+
     def get_arrival(self):
+        '''Returnes the second arrival of a voyage on iso format'''
         return self.__second_arrival
 
     def get_airplane_name(self):
+        '''Returnes the plane id of a voyage'''
         return self.__aircraft_id
+    
+    def add_dates_to_voyage(self, first_arrival_date, second_departure_date, second_arrival_date):
+        '''Takes three dates on iso format and assigns it to the date variables'''
+        self.__first_arrival = first_arrival_date
+        self.__second_departure = second_departure_date
+        self.__second_arrival = second_arrival_date
+    
+    def add_flight_numbers_to_voyage(self, num1, num2):
+        '''Takes two flight numbers on string format and assigns it to the flight number variables'''
+        self.first_flight_number = num1
+        self.second_flight_number = num2
+    
+    def add_number_of_seats(self, number_of_seats):
+        '''Takes a number of seats for a plane on string format and assigns it to the seat variable'''
+        self.seats = number_of_seats
+
+    def change_flight_numbers(self):
+        '''Takes an instance of a Voyage and raises the last number in the flight numbers by 2'''
+        last_num_1 = int(self.first_flight_number[4])
+        last_num_2 = int(self.second_flight_number[4])
+
+        last_char_1 = str(last_num_1 + 2)
+        last_char_2 = str(last_num_2 + 2)
+
+        self.first_flight_number = self.first_flight_number[:4] + last_char_1
+        self.second_flight_number = self.second_flight_number[:4] + last_char_2
