08ad2a64f2cc6122c47fddd85b25899e398f13f5
Breki Ben <brekiben@Brekis-MacBook-Pro.local>
2019-12-13 13:24:11 +0000

    BiGmAcIsBaCk

--- Logic/AirplaneLL.py
+++ Logic/AirplaneLL.py
@@ -11,14 +11,3 @@ class AirplaneLL:
     def list_airplanes(self):
         return self.__data_layer.list_airplanes()
 
-    def change_airplane(self, planeInsignia, what_to_change, new_info):
-        all_airplanes = self.__data_layer.list_airplanes()
-        for num in range(len(all_airplanes)):
-            if all_airplanes[num] == planeInsignia:
-                if what_to_change == 'planeInsignia':
-                    all_airplanes[num].name = new_info
-                elif what_to_change == 'PlaneTypeId':
-                    all_airplanes[num].model = new_info
-                self.__data_layer.overwrite_airplane_file(all_airplanes)
-            else:
-                input("Airplane not found, press enter to return to main menu...")
