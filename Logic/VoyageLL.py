94c897bb29c361bb5b849c0110f1fc86e347840b
Anna Sigrun <annasg19@ru.is>
2019-12-13 12:33:49 +0000

    fuk

--- Logic/VoyageLL.py
+++ Logic/VoyageLL.py
@@ -68,11 +68,9 @@ class VoyageLL:
 
     def find_voyage(self, flight_num, date):
         all_voyages = self.__data_layer.list_voyages()
-        found_voyages = []
         for voyage in all_voyages:
             if (date == voyage.get_voyage_depart_time()) and (flight_num in voyage.get_voyage_flight_numbers()):
-                found_voyages.append(voyage)
-        return found_voyages
+                return found_voyages
 
     def get_all_voyages_by_date(self, from_date, to_date):
         from_date = dateutil.parser.parse(from_date).date()
